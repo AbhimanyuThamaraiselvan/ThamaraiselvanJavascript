f22d301e5beb3f42f9f258c2275c8fdf63a4f3bd
AbhimanyuThamaraiselvan <itstamaraimani@gmail.com>
2023-12-17 22:56:05 +0530

    line 5 added

--- readme.js
+++ readme.js
@@ -1,6 +1,9 @@
-/* 1. login github page
+/* 
+
+1. login github page
 2. create repositary
 3. initialize
 4. create file
+5. sync cloud
 
 */
---
+++ redm.md
@@ -0,0 +1,5 @@
+/* 
+I am checkeing for upload in cloud
+
+
+*/
